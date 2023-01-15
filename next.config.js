7714a1f94b95f2021d52767ea55f7c2ac1931d89
hasindusithmin <hasindusithmin64@gmail.com>
2023-01-15 14:36:36 +0530

    Release v:0.5.0-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest:"public",
+  register:true,
+  skipWaiting:true,
+  disable: process.env.NEXT_PUBLIC_ENVIROMENT === 'development'
+})
+
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  swcMinify: true,
+})
 
 module.exports = nextConfig
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="FuelLine" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="FuelLine" />
+        <meta name="description" content="Petrol Queue Management APP" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-192x192.png' />
+        <meta name='theme-color' content='#fff' />
+      </Head>
       <body>
         <Main />
         <NextScript />
